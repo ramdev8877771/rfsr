6c74ada2c88d20d079d25d49f459319638f5986f
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-02-03 03:28:57 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import subs from './subs'
 
 function App() {
+  const count = 0
   const handleClick = () => {
     const randSub = subs[Math.floor(Math.random() * (14999 + 1))]
+    count += 1
 
     window.location = `https://redditfavorites.com/r/${randSub}`
   }
-   setTimeout(handleClick, 1000)
+  if(count != 0){
+    setTimeout(handleClick, 1000)
+  }
   return (
     <div className='App' onClick={handleClick}>
       <h1>Click Anywhere</h1>
