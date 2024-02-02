c61df4044e79470f2365639d5bccb6d88d230b87
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-02-03 03:37:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import React from 'react'
 import subs from './subs'
 
 function App() {
-  localStorage.setItem("count", 0)
   const handleClick = () => {
     const randSub = subs[Math.floor(Math.random() * (14999 + 1))]
-    localStorage.setItem("count", 1)
 
     window.location = `https://redditfavorites.com/r/${randSub}`
-  }
-  if(localStorage.getItem("count") !== 0){
-    setTimeout(handleClick, 500)
   }
   return (
     <div className='App' onClick={handleClick}>
