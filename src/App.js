38c3bf602cbb4b6cb0add1edc515583f5516bb46
ramdev8877771 <156853367+ramdev8877771@users.noreply.github.com>
2024-03-02 21:06:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import subs from './subs'
 
 function App() {
   const handleClick = () => {
-    const randSub = subs[Math.floor(Math.random() * 74999)]
+    const randSub = subs[Math.floor(Math.random() * 15000 + 15000)]
 
     window.location = `https://redditfavorites.com/r/${randSub}`
   }
